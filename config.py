b143c26ae7c05265963562e77afdc24b99340a71
Aditi Kamatgi <aditihk@gmail.com>
2024-12-10 22:23:46 -0700

    added cart api

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://tea_user:tea123@localhost:5432/tea_shop'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://tea_user:tea123@localhost:5432/tea_shop'
+SQLALCHEMY_DATABASE_URI = 'postgresql://tea_user:tea_123@35.193.72.71:5432/teashop'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 REDIS_URL = 'redis://localhost:6379/0'
 MAIL_USERNAME = 'tearoma.help'
 MAIL_PASSWORD = 'apbg nkqe ndzf ljhl'
 SECRET_KEY = 'QiwdbYdxj98'
+
+CLOUD_SQL_USERNAME='tea_user'
+CLOUD_SQL_PASSWORD='tea_123'
+CLOUD_SQL_DATABASE='teashop'
+CLOUD_SQL_CONNECTION_NAME='adroit-solstice-438002-d8:us-central1:teashop'
